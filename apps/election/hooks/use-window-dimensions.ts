c7fa782f14c9008868cc7c36611b2c588974419a
Thanonchai Paliwong <t.paliwong@gmail.com>
2024-08-24 22:00:23 +0700

    first commit

---
+++ apps/election/hooks/use-window-dimensions.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+
+export const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+};
---
+++ apps/election/utils/breakpoints.ts
@@ -0,0 +1,7 @@
+export const breakpoints = {
+  0: 'xs',
+  600: 'sm',
+  960: 'md',
+  1280: 'lg',
+  1920: 'xl',
+};
